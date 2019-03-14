5e2d9c1777dc3d69503b513823f4cbdb2e93b0ed
charles-desouza <charles5388@gmail.com>
2019-03-14 15:16:44 +0530

    First commit

---
+++ ExpenseTracker.Api/Models/ExpenseItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.Api.Models
+{
+  public class ExpenseItem
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Amount { get; set; }
+    public int CategoryId { get; set; }
+    public DateTime DateTime { get; set; }
+
+    public ExpenseCategory Category { get; set; }
+  }
+}
