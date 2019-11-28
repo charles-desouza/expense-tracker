324e95455ccf76b8d617c04aabbbedd9793cb71f
charles-desouza <charles5388@gmail.com>
2019-11-28 17:38:16 +0530

    Updated view

---
+++ ExpenseTracker.Web/Controllers/ExpensesController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ExpenseTracker.Web.Data;
+using ExpenseTracker.Web.Models;
+
+namespace ExpenseTracker.Web.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly ExpenseDbContext _context;
+
+        public ExpensesController(ExpenseDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Expenses
+        public async Task<IActionResult> Index()
+        {
+            var expenseDbContext = _context.Expenses.Include(e => e.ExpenseCategory).Include(e => e.PaymentType);
+            return View(await expenseDbContext.ToListAsync());
+        }
+
+        // GET: Expenses/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses
+                .Include(e => e.ExpenseCategory)
+                .Include(e => e.PaymentType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return View(expense);
+        }
+
+        // GET: Expenses/Create
+        public IActionResult Create()
+        {
+            ViewData["ExpenseCategoryId"] = new SelectList(_context.ExpenseCategories, "Id", "Name");
+            ViewData["PaymentTypeId"] = new SelectList(_context.PaymentTypes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Expenses/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,ExpenseCategoryId,PaymentTypeId,Amount")] Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(expense);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ExpenseCategoryId"] = new SelectList(_context.ExpenseCategories, "Id", "Id", expense.ExpenseCategoryId);
+            ViewData["PaymentTypeId"] = new SelectList(_context.PaymentTypes, "Id", "Id", expense.PaymentTypeId);
+            return View(expense);
+        }
+
+        // GET: Expenses/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses.FindAsync(id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+            ViewData["ExpenseCategoryId"] = new SelectList(_context.ExpenseCategories, "Id", "Id", expense.ExpenseCategoryId);
+            ViewData["PaymentTypeId"] = new SelectList(_context.PaymentTypes, "Id", "Id", expense.PaymentTypeId);
+            return View(expense);
+        }
+
+        // POST: Expenses/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ExpenseCategoryId,PaymentTypeId,Amount")] Expense expense)
+        {
+            if (id != expense.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(expense);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExpenseExists(expense.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ExpenseCategoryId"] = new SelectList(_context.ExpenseCategories, "Id", "Id", expense.ExpenseCategoryId);
+            ViewData["PaymentTypeId"] = new SelectList(_context.PaymentTypes, "Id", "Id", expense.PaymentTypeId);
+            return View(expense);
+        }
+
+        // GET: Expenses/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var expense = await _context.Expenses
+                .Include(e => e.ExpenseCategory)
+                .Include(e => e.PaymentType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (expense == null)
+            {
+                return NotFound();
+            }
+
+            return View(expense);
+        }
+
+        // POST: Expenses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var expense = await _context.Expenses.FindAsync(id);
+            _context.Expenses.Remove(expense);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExpenseExists(int id)
+        {
+            return _context.Expenses.Any(e => e.Id == id);
+        }
+    }
+}
